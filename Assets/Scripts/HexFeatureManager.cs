91eaaec476c276fa5574ab941ae6dc39da632510
Ian Laidlaw <ian@kaboodles.com>
2021-02-19 15:18:12 -0500

    Initial Commit- up to the beginning of part 9 of tutorial when adding red cubes placed on top of each hex

---
+++ Assets/Scripts/HexFeatureManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HexFeatureManager : MonoBehaviour
+{
+    public Transform featurePrefab;
+
+    public void Clear() { }
+
+    public void Apply() { }
+
+    public void AddFeature(Vector3 position)
+    {
+        Transform instance = Instantiate(featurePrefab);
+        position.y += instance.localScale.y * 0.5f;
+        instance.localPosition = position;
+    }
+}
